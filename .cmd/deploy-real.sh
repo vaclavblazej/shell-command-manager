7c963f825a253c2c0158b513c28951394bf950eb
Vaclav Blazej <blazeva1@fit.cvut.cz>
2020-05-11 01:07:42 +0200

    deployment settings

---
+++ .cmd/deploy-real.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cd $project_root
+
+echo 'Deploying to the PUBLIC pypi server!'
+echo 'Content to be deployed:'
+ls ./dist/
+echo 'Are you sure you want to continue? (y/N): '
+read res
+echo $res
+if [ "$res" == 'y' ]; then
+    python3 -m twine upload dist/* \
+    && echo 'Deployment successful'
+else
+    echo 'Deployment canceled'
+fi
+
--- .cmd/deploy.sh
+++ .cmd/deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+cd $project_root
+
 echo 'Deploying to the TEST pypi server'
 python3 -m twine upload --repository testpypi dist/* \
 && echo 'Deployment successful'
